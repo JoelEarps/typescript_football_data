1ac572e2ef17b384ccc585a07a78fe96bbe96797
Joel Earps <joel.earps@zenobe.com>
2022-11-09 17:29:15 +0000

    feat! Split football csv and get 2d array of data points

--- build/index.js
+++ build/index.js
@@ -1,3 +1,23 @@
 "use strict";
-console.log("Test");
-console.log("Hello");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+// Cannot find fs unless you have a type def file
+// TS reporting error incorrectly for us
+// you require anything inside node standard library you need to install
+// @types/node
+// With a csv - first split variables by new line
+// Then we shall make a 2d array to get values of each column in the newly split row
+// you need to delimited by comma (hence comma seperated)
+const matches = fs_1.default
+    .readFileSync(path_1.default.resolve(__dirname, "../football.csv"), {
+    encoding: "utf-8",
+})
+    .split("\n")
+    .map((row) => {
+    return row.split(",");
+});
+console.log(matches);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-console.log("Test");
-console.log("Hello");
+import fs from "fs";
+import path from "path";
+// Cannot find fs unless you have a type def file
+// TS reporting error incorrectly for us
+// you require anything inside node standard library you need to install
+// @types/node
+
+// With a csv - first split variables by new line
+// Then we shall make a 2d array to get values of each column in the newly split row
+// you need to delimited by comma (hence comma seperated)
+const matches = fs
+  .readFileSync(path.resolve(__dirname, "../football.csv"), {
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
+
+console.log(matches);
