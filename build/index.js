3ee50f425985980b21067f4a8bda09d489d42ecd
Joel Earps <joel.earps@zenobe.com>
2022-11-09 18:37:47 +0000

    fix!: Using Enums

--- build/index.js
+++ build/index.js
@@ -23,11 +23,19 @@ const matches = fs_1.default
 console.log(matches);
 // initial analysis - count number of times man united have won
 let manUtdWin = 0;
+// enums - enumeration
+// Used simply to signal to other engineers that this is a collection to many closely related values
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
 for (let match of matches) {
-    if (match[1] === "Man United" && match[5] === "H") {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
         manUtdWin++;
     }
-    else if (match[2] === "Man United" && match[5] === "A") {
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
         manUtdWin++;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -22,11 +22,19 @@ console.log(matches);
 // initial analysis - count number of times man united have won
 
 let manUtdWin: number = 0;
+// enums - enumeration
+// Used simply to signal to other engineers that this is a collection to many closely related values
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
 
 for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUtdWin++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUtdWin++;
   }
 }
